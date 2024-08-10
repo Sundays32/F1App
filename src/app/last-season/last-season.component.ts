23bd01c993fe1760c4497d9ecb956458a464ff19
Sandesh <sandy.2211.r@gmail.com>
2024-08-10 19:07:27 +0530

    last season page added

--- src/app/last-season/last-season.component.ts
+++ src/app/last-season/last-season.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import {MatTableDataSource, MatTableModule} from '@angular/material/table';
+import * as driverStandings from './driverStandings.json'
+import * as constructorsStandings from './constructorsStandings.json'
 
 @Component({
   selector: 'app-last-season',
@@ -6,5 +9,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./last-season.component.scss']
 })
 export class LastSeasonComponent {
-
+  displayedColumns: string[] = ['rank', 'country', 'driver','team','points','wins','podiums'];
+  displayedColumnsConst: string[] = ['rank', 'team', 'points','wins','podiums'];
+  dataSource = new MatTableDataSource(driverStandings);
+  dataSourceConstChamp = new MatTableDataSource(constructorsStandings);
 }
