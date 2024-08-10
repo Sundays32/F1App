23409a6cff33c54d433d5e41a783908443a785d7
Sandesh <sandy.2211.r@gmail.com>
2024-08-10 17:19:14 +0530

    history and last seaon

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LandingPageComponent } from './landing-page/landing-page.component';
+import { HistoryComponent} from './history/history.component'
 import { TeamsComponent } from './teams/teams.component';
 import { RedbullComponent } from './teams/redbull/redbull.component';
 import { MercedesComponent } from './teams/mercedes/mercedes.component';
@@ -12,10 +13,13 @@ import { AlpineComponent } from './teams/alpine/alpine.component';
 import { AstonMartinComponent } from './teams/aston-martin/aston-martin.component';
 import { MclarenComponent } from './teams/mclaren/mclaren.component';
 import { FerrariComponent } from './teams/ferrari/ferrari.component';
+import { LastSeasonComponent } from './last-season/last-season.component';
 
 const routes: Routes = [
   {path:'', redirectTo:'landing-page', pathMatch:'full'},
-  { path:'landing-page', component: LandingPageComponent },
+  { path: 'landing-page', component: LandingPageComponent },
+  { path: 'history', component: HistoryComponent },
+  { path: 'last-season', component: LastSeasonComponent },
   { path: 'teams-page', component: TeamsComponent },
   { path: 'RedBull', component: RedbullComponent },
   { path: 'Mercedes', component: MercedesComponent },
